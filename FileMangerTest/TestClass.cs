c18d88409452be1a77035dcd520e528866d3029a
mansoureh69 <mansourehesmaeili69@gmail.com>
2019-11-01 11:32:18 +0330

    cmmitpackage

--- FileMangerTest/TestClass.cs
+++ FileMangerTest/TestClass.cs
@@ -1,44 +1,44 @@
-﻿namespace FileMangerTest
-{
-    using System;
-    using Microsoft.VisualStudio.TestTools.UnitTesting;
-    namespace FileManagerUnitTest
-    {
+﻿//namespace FileMangerTest
+//{
+//    using System;
+//    using Microsoft.VisualStudio.TestTools.UnitTesting;
+//    namespace FileManagerUnitTest
+//    {
 
-        [TestClass]
-        public class FileSystemTests
-        {
-            public User User { get; set; }
-            [TestMethod]
-            public void AddFileToFolder_Success()
-            {
-                var file = new File
-                {
-                    CreationDateTime = DateTime.Now,
-                    LastModifiedDateTime = DateTime.Now,
-                    CreatorUser = User,
-                    Size = 100
-                };
-                var folder = new Folder
-                {
-                    CreationDateTime = DateTime.Now,
-                    LastModifiedDateTime = DateTime.Now,
-                    CreatorUser = User
-                };
-                folder.AddSubFileSystem(file);
-                var parentFolder = new Folder
-                {
-                    CreationDateTime = DateTime.Now,
-                    LastModifiedDateTime = DateTime.Now,
-                    CreatorUser = User
-                };
-                parentFolder.AddSubFileSystem(folder);
-                parentFolder.AddSubFileSystem(file);
-                var t = parentFolder.GetSize();
-                Assert.AreEqual(t,200);
-            }
+//        [TestClass]
+//        public class FileSystemTests
+//        {
+//            public User User { get; set; }
+//            [TestMethod]
+//            public void AddFileToFolder_Success()
+//            {
+//                var file = new File
+//                {
+//                    CreationDateTime = DateTime.Now,
+//                    LastModifiedDateTime = DateTime.Now,
+//                    CreatorUser = User,
+//                    Size = 100
+//                };
+//                var folder = new Folder
+//                {
+//                    CreationDateTime = DateTime.Now,
+//                    LastModifiedDateTime = DateTime.Now,
+//                    CreatorUser = User
+//                };
+//                folder.AddSubFileSystem(file);
+//                var parentFolder = new Folder
+//                {
+//                    CreationDateTime = DateTime.Now,
+//                    LastModifiedDateTime = DateTime.Now,
+//                    CreatorUser = User
+//                };
+//                parentFolder.AddSubFileSystem(folder);
+//                parentFolder.AddSubFileSystem(file);
+//                var t = parentFolder.GetSize();
+//                Assert.AreEqual(t,200);
+//            }
 
-        }
-    }
+//        }
+//    }
 
-}
+//}
